669cd84e81f9181b49fe57f1fed1be5c2203f6a4
BlackPuma9 <yuliiayavorska9@gmail.com>
2023-10-01 11:12:06 +0200

    Completed homework 9

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 'use strict'
+
+// Boolean(Number('10')) + 1
+// Answer: 2
+
+// 'sub' + Number(false);
+// Answer: 'sub0'
+
+// 16 * '91'
+// Answer: 1456
+
+// true-70
+// Answer: -69
+
+// Number(1 + String(1)) + 1
+// Answer: 12
