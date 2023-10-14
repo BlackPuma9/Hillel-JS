8fbb080872628aea41950f6b73672c178cfe67ca
BlackPuma9 <yuliiayavorska9@gmail.com>
2023-10-14 14:00:30 +0200

    Completed homework 20

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 'use strict'
+
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789'.split('')
+
+function generateKey(length, characters) {
+    const randomise = [...characters].sort(() => 0.5 - Math.random())
+
+    return randomise.slice(0, length).join('')
+}
+
+const key = generateKey(16, characters)
+
+console.log(key) // eg599gb60q926j8i
