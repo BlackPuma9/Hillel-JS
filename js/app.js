3b6b1b849e52b7be53e9060ce21eb2fea0177a88
BlackPuma9 <yuliiayavorska9@gmail.com>
2023-10-15 13:12:14 +0200

    Completed hw22 + refactoring

--- js/app.js
+++ js/app.js
@@ -1,37 +1,40 @@
 'use strict'
 
-const padString = function (str, num, symbol, side = true) {
-    if (arguments.length < 4 || arguments.length > 4) {
-        console.log('4 parameters requested')
-    }
-    if (typeof arguments[0] !== 'string') {
+const padString = function (str, num, symbol = '*', fromStart = true) {
+    if (typeof str !== 'string') {
         return 'First argument should be string'
     }
-    if (typeof arguments[1] !== 'number') {
+    if (typeof num !== 'number') {
         return 'Second argument should be number'
     }
-    if (typeof arguments[2] !== 'string') {
-        console.log('Third argument should be string')
+    if (typeof symbol !== 'string') {
+        return 'Third argument should be string'
     }
-    if (typeof arguments[3] !== 'boolean') {
-        console.log('Last argument should be boolean')
+    if (symbol.length > 1) {
+        return 'Third argument should be one symbol'
+    }
+    if (typeof fromStart !== 'boolean') {
+        return 'Last argument should be boolean'
     }
 
-    let argOne = arguments[0].split('')
-    const rounds = num - argOne.length
+    let result = str.split('')
+    const rounds = num - result.length
 
-    if (argOne.length >= num) {
-        booleanValidation(side, symbol, argOne)
-        return argOne.slice(0, num).join('')
+    if (str.length >= num) {
+        buildResult(fromStart, symbol, result)
+    } else {
+        for (let i = 1; i <= rounds; i++) {
+            buildResult(fromStart, symbol, result)
+        }
     }
-    for (let i = 1; i <= rounds; i++) {
-        booleanValidation(side, symbol, argOne)
-    }
-    return argOne.slice(0, num).join('')
+
+    return result.slice(0, num).join('')
 }
 
-function booleanValidation(side, symbol, argOne) {
-    return side === true ? argOne.push(symbol) : argOne.unshift(symbol)
+function buildResult(side, symbol, argOne) {
+    side ? argOne.push(symbol) : argOne.unshift(symbol)
 }
 
-console.log(padString('hello', 8, '*'))
+console.log(padString('hello', 2, '*', false))
+// console.log(padString('hello', 2, '*', true))
+// console.log(padString('hello', 18))
