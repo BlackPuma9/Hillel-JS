d37d9cc3a36aafc2299f32ec6dba152412f6a459
BlackPuma9 <yuliiayavorska9@gmail.com>
2023-11-05 13:38:07 +0100

    Completed hw31

--- js/app.js
+++ js/app.js
@@ -1,1 +1,43 @@
 'use strict'
+
+const counter = (val = 0) => {
+    return {
+        counter: val,
+        valueCounter: 0,
+        increaseCounter: 0,
+        decreaseCounter: 0,
+        value() {
+            this.valueCounter++
+            return this.counter
+        },
+        increase() {
+            this.increaseCounter++
+            this.counter++
+        },
+        decrease() {
+            this.decreaseCounter++
+            this.counter--
+        },
+        getStatistic() {
+            return {
+                increase: this.increaseCounter,
+                decrease: this.decreaseCounter,
+                value: this.valueCounter,
+            }
+        },
+        reset() {
+            this.counter = 0
+            this.valueCounter = 0
+            this.increaseCounter = 0
+            this.decreaseCounter = 0
+        },
+    }
+}
+
+const counter1 = counter(9)
+console.log(counter1.value())
+counter1.decrease()
+console.log(counter1.value())
+console.log(counter1.getStatistic())
+counter1.reset()
+console.log(counter1.getStatistic())
