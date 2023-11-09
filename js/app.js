978675d7f32d52aa765ae885aca9506d83aa9b82
BlackPuma9 <yuliiayavorska9@gmail.com>
2023-11-09 20:27:47 +0100

    Completed hw32

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
 'use strict'
+
+const cat = {
+    name: 'Busia',
+    surname: 'Kote',
+}
+
+const dog = {
+    name: 'Biba',
+    surname: 'Boba',
+}
+
+function getFullName(age) {
+    return console.log(
+        `Hi! I am ${this.name} ${this.surname} and I have ${age}`
+    )
+}
+
+function bind(func, context, ...rest) {
+    return function (...args) {
+        context.addFunc = func
+        const cumulatedArg = rest.concat(args)
+        const result = context.addFunc(...cumulatedArg)
+        delete context.addFunc
+        return result
+    }
+}
+
+bind(getFullName, cat, 10)()
+bind(getFullName, cat)(2)
+bind(getFullName, dog, 7)()
