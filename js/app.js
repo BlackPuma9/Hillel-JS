2ac25cddb5d04d174b613e288484b71689d05d64
BlackPuma9 <yuliiayavorska9@gmail.com>
2023-11-15 19:22:47 +0100

    IIFE function added

--- js/app.js
+++ js/app.js
@@ -1,31 +1,30 @@
 'use strict'
-
-const cat = {
-    name: 'Busia',
-    surname: 'Kote',
-}
-
-const dog = {
-    name: 'Biba',
-    surname: 'Boba',
-}
-
-function getFullName(age) {
-    return console.log(
-        `Hi! I am ${this.name} ${this.surname} and I have ${age}`
-    )
-}
-
-function bind(func, context, ...rest) {
-    return function (...args) {
-        context.addFunc = func
-        const cumulatedArg = rest.concat(args)
-        const result = context.addFunc(...cumulatedArg)
-        delete context.addFunc
-        return result
+;(function () {
+    const cat = {
+        name: 'Busia',
+        surname: 'Kote',
     }
-}
 
-bind(getFullName, cat, 10)()
-bind(getFullName, cat)(2)
-bind(getFullName, dog, 7)()
+    const dog = {
+        name: 'Biba',
+        surname: 'Boba',
+    }
+
+    function printFullName(age) {
+        console.log(`Hi! I am ${this.name} ${this.surname} and I have ${age}`)
+    }
+
+    function bind(func, context, ...rest) {
+        return function (...args) {
+            context.addFunc = func
+            const cumulatedArg = rest.concat(args)
+            const result = context.addFunc(...cumulatedArg)
+            delete context.addFunc
+            return result
+        }
+    }
+
+    bind(printFullName, cat, 10)()
+    bind(printFullName, cat)(2)
+    bind(printFullName, dog, 7)()
+})()
