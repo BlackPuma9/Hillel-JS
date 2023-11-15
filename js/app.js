7e73abb00c0b2565d99ba36dfdf006febd207407
BlackPuma9 <yuliiayavorska9@gmail.com>
2023-11-15 20:03:01 +0100

    IIFE function added

--- js/app.js
+++ js/app.js
@@ -1,30 +1,31 @@
-'use strict'
-;(function () {
+'use strict';
+
+(function () {
     const cat = {
         name: 'Busia',
         surname: 'Kote',
-    }
+    };
 
     const dog = {
         name: 'Biba',
         surname: 'Boba',
-    }
+    };
 
     function printFullName(age) {
-        console.log(`Hi! I am ${this.name} ${this.surname} and I have ${age}`)
+        console.log(`Hi! I am ${this.name} ${this.surname} and I have ${age}`);
     }
 
     function bind(func, context, ...rest) {
         return function (...args) {
-            context.addFunc = func
-            const cumulatedArg = rest.concat(args)
-            const result = context.addFunc(...cumulatedArg)
-            delete context.addFunc
-            return result
-        }
+            context.addFunc = func;
+            const cumulatedArg = rest.concat(args);
+            const result = context.addFunc(...cumulatedArg);
+            delete context.addFunc;
+            return result;
+        };
     }
 
-    bind(printFullName, cat, 10)()
-    bind(printFullName, cat)(2)
-    bind(printFullName, dog, 7)()
-})()
+    bind(printFullName, cat, 10)();
+    bind(printFullName, cat)(2);
+    bind(printFullName, dog, 7)();
+})();
