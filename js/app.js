0dc2a2362a029a2e3a4fd7eefeb64b6fc5b4c662
BlackPuma9 <yuliiayavorska9@gmail.com>
2023-11-18 17:01:58 +0100

    Completed hw 34

--- js/app.js
+++ js/app.js
@@ -1,25 +1,47 @@
 'use strict'
 
-function insertWordInto(phrase = '') {
+function insertWordInto(str) {
     const cash = []
     const creatingNew = (word) => {
-        const insertStart = `${word} ${phrase}`
-        if (!cash.includes(insertStart)) {
-            cash.push(insertStart)
-            return insertStart
-        }
-        const insertBack = `${phrase} ${word}`
-        if (!cash.includes(insertBack)) {
-            cash.push(insertBack)
-            return insertBack
+        const indexes = [0, str.length - 1]
+        for (let i = 0; i < str.length; i++) {
+            if (str[i] === ' ') {
+                indexes.push(i)
+            }
         }
 
-        for (let i = 0; i < phrase.length; i++) {
-            if (phrase[i] === ' ') {
-                const newPhrase = `${phrase.substring(
+        const randomIndexes = indexes.sort(() => Math.random() - 0.5)
+
+        if (typeof word !== 'string') {
+            throw new TypeError('Argument should be a string')
+        }
+        if (word.length === 0) {
+            throw new Error('Argument can not be empty')
+        }
+
+        for (let i = 0; i < randomIndexes.length; i++) {
+            if (randomIndexes[i] === 0) {
+                const insertStart = `${word} ${str}`
+                if (!cash.includes(insertStart)) {
+                    cash.push(insertStart)
+                    return insertStart
+                }
+            }
+
+            if (randomIndexes[i] === str.length - 1) {
+                const insertBack = `${str} ${word}`
+                if (!cash.includes(insertBack)) {
+                    cash.push(insertBack)
+                    return insertBack
+                }
+            }
+
+            if (randomIndexes[i] !== 0 && randomIndexes[i] !== str.length - 1) {
+                const newPhrase = `${str.substring(
                     0,
-                    i
-                )} ${word} ${phrase.substring(i + 1)}`
+                    randomIndexes[i]
+                )} ${word}${str.substring(randomIndexes[i])}`
+
                 if (!cash.includes(newPhrase)) {
                     cash.push(newPhrase)
                     return newPhrase
@@ -30,7 +52,6 @@ function insertWordInto(phrase = '') {
         cash.length = 0
         return creatingNew(word)
     }
-
     return creatingNew
 }
 
@@ -38,9 +59,9 @@ const insert = insertWordInto('hello world')
 const result = insert('Odesa')
 const secondResult = insert('Odesa')
 const thirdResult = insert('Odesa')
-const fifth = insert('Odesa')
+const fourthResult = insert('Odesa')
 
 console.log(result)
 console.log(secondResult)
 console.log(thirdResult)
-console.log(fifth)
+console.log(fourthResult)
