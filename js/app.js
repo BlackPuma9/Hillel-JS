7fc0813a75942dcd19f4a88ed6ea4772af61dc96
BlackPuma9 <yuliiayavorska9@gmail.com>
2023-11-19 13:17:27 +0100

    First version of the task

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
 'use strict'
+
+const createTable = () => {
+    const table = document.createElement('table')
+    const tbody = document.createElement('tbody')
+    let counter = 1
+    for (let r = 0; r < 10; r++) {
+        const tr = document.createElement('tr')
+        for (let d = 0; d < 10; d++) {
+            const td = document.createElement('td')
+            td.textContent = `${counter}`
+            tr.appendChild(td)
+            counter++
+        }
+        tbody.appendChild(tr)
+    }
+    table.appendChild(tbody)
+    document.body.prepend(table)
+}
+createTable()
