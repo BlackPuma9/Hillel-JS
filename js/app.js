921f6fc6b29eec2d7f0785f2826ed4f4f1e76274
BlackPuma9 <yuliiayavorska9@gmail.com>
2023-11-19 12:22:52 +0100

    Completed hw37

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
 'use strict'
+
+const generateList = (array) => {
+    const ul = document.createElement('ul')
+
+    for (let i = 0; i < array.length; i++) {
+        let li = document.createElement('li')
+
+        if (Array.isArray(array[i])) {
+            li.appendChild(generateList(array[i]))
+        } else {
+            li.textContent = `Element ${array[i]}`
+        }
+
+        ul.appendChild(li)
+    }
+    return ul
+}
+const nestedList = generateList([1, 2, [1.1, 1.2, 1.3], 3])
+document.body.prepend(nestedList)
