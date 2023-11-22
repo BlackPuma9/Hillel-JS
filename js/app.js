a823efa8e216c592c43af6d5aeb17fe50bffe034
BlackPuma9 <yuliiayavorska9@gmail.com>
2023-11-22 21:16:25 +0100

    Completed hw39

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 'use strict'
+
+const input = document.querySelector('[data-input]')
+
+input.addEventListener('focus', () => {
+    document.querySelector('[data-msg-error]').style.display = 'inline-block'
+})
+
+input.addEventListener('blur', () => {
+    document.querySelector('[data-msg-error]').style.display = 'none'
+})
