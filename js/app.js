7a32ed4ef20522ecd68a08d02f2252ce44a079bc
BlackPuma9 <yuliiayavorska9@gmail.com>
2023-11-22 22:42:23 +0100

    Completed hw40

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 'use strict'
+
+const random = Math.floor(Math.random() * 10 + 1)
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('[data-img]').src = `./img/${random}.jpg`
+})
+
+// Version 2
+// ;(() => {
+//     const random = Math.floor(Math.random() * 10 + 1)
+//     document.querySelector('[data-img]').src = `./img/${random}.jpg`
+// })()
