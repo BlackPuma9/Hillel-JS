30f4ce5e5bd81deb196392b91cd24dc02009449a
BlackPuma9 <yuliiayavorska9@gmail.com>
2023-11-25 20:12:46 +0100

    A draft of Image Gallery ready

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
 'use strict'
+;(() => {
+    const modalWindow = document.getElementById('modalWindow')
+    const container = document.getElementById('container')
+    const modalImg = document.getElementById('modalImage')
+    const imgText = document.getElementById('imgText')
+
+    container.addEventListener('click', (event) => {
+        modalWindow.style.display = 'block'
+        modalImg.src = event.target.src
+        imgText.innerHTML = event.target.alt
+    })
+
+    const closeBtn = document.getElementById('closeBtn')
+
+    closeBtn.addEventListener('click', () => {
+        modalWindow.style.display = 'none'
+    })
+})()
