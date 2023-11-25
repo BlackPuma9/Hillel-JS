b8a771373359cd802869cdaa7c103614b11ef0ff
BlackPuma9 <yuliiayavorska9@gmail.com>
2023-11-26 00:04:26 +0100

    Pre-version of 2nd task,hw41

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 'use strict'
+;(() => {
+    const tab = document.querySelector('[data-tab]')
+    tab.addEventListener('click', (event) => {
+        const tabContentId = event.target.getAttribute('href')
+
+        document.querySelectorAll('.tabContent').forEach((item) => {
+            item.style.display = 'none'
+        })
+
+        const newId = tabContentId.slice(1)
+        document.getElementById(newId).style.display = 'block'
+    })
+})()
