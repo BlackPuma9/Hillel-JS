811cc07e82839a5db731b562b90104f3ecfaa7c7
BlackPuma9 <yuliiayavorska9@gmail.com>
2023-11-26 14:02:05 +0100

    Completed hw41.2

--- js/app.js
+++ js/app.js
@@ -2,13 +2,16 @@
 ;(() => {
     const tab = document.querySelector('[data-tab]')
     tab.addEventListener('click', (event) => {
-        const tabContentId = event.target.getAttribute('href')
+        document.querySelectorAll('li a').forEach((element) => {
+            element.style.backgroundColor = ''
+        })
+        event.target.style.backgroundColor = '#303030'
 
         document.querySelectorAll('.tabContent').forEach((item) => {
             item.style.display = 'none'
         })
 
-        const newId = tabContentId.slice(1)
-        document.getElementById(newId).style.display = 'block'
+        const tabContentId = event.target.getAttribute('href').slice(1)
+        document.getElementById(tabContentId).style.display = 'block'
     })
 })()
