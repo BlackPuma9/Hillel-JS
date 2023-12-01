6da50235edd5c013f7e498c799c6453f2aeefeb4
BlackPuma9 <yuliiayavorska9@gmail.com>
2023-12-01 23:56:09 +0100

    First version of the file

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
 'use strict'
+;(function () {
+    const saveData = (data = null) => {
+        localStorage.setItem('formData', JSON.stringify(data))
+    }
+    const formHandler = (event) => {
+        event.stopPropagation()
+        event.preventDefault()
+        const { target } = event
+        const data = {}
+        target
+            .querySelectorAll('input, textarea, select')
+            .forEach((item) => (data[item.name] = item.value))
+        saveData(data)
+        target.reset()
+    }
+
+    const loadedHandler = (event) => {
+        const form = document.querySelector('#form')
+        form.addEventListener('submit', formHandler)
+    }
+
+    document.addEventListener('DOMContentLoaded', loadedHandler)
+})()
