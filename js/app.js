7c2d5d1af2d628b4f410a9546c0c16f8553c9f3d
BlackPuma9 <yuliiayavorska9@gmail.com>
2023-12-03 16:35:54 +0100

    Completed hw 43. Task 2 with 2 btns vertions

--- js/app.js
+++ js/app.js
@@ -1,87 +1,56 @@
 'use strict'
 ;(function () {
-    // const restoreFromState = () => {
-    //     const data = getWishes()
-    //     const btns = document.querySelectorAll('[data-id]')
-    //     btns.forEach((btn) => {
-    //         const id = btn.getAttribute('data-id')
-    //         if (data.includes(id)) {
-    //             btn.textContent = 'DELETE'
-    //             toggleClasses(btn)
-    //         }
-    //     })
-    // }
-    // const toggleClasses = (element) => {
-    //     element.classList.toggle('btn-success')
-    //     element.classList.toggle('btn-danger')
-    // }
-    //
-    // const getWishes = () => {
-    //     const wishes = localStorage.getItem('wishes')
-    //     return wishes === null ? [] : JSON.parse(wishes)
-    // }
-    //
-    // const clickHandler = (event) => {
-    //     const { target } = event
-    //     if (
-    //         target.classList.contains('btn-danger') ||
-    //         target.classList.contains('btn-success')
-    //     ) {
-    //         let data = getWishes()
-    //         const id = event.target.getAttribute('data-id')
-    //         if (data.includes(id)) {
-    //             data = data.filter((item) => item !== id)
-    //             target.textContent = 'ADD'
-    //             toggleClasses(target)
-    //         } else {
-    //             data.push(id)
-    //             target.textContent = 'DELETE'
-    //             toggleClasses(target)
-    //         }
-    //         localStorage.setItem('wishes', JSON.stringify(data))
-    //     }
-    // }
+    const restoreFromState = () => {
+        const data = getLocalStore()
+        const btns = document.querySelectorAll('[data-id]')
+        btns.forEach((btn) => {
+            const id = btn.getAttribute('data-id')
+            if (data.hasOwnProperty(id)) {
+                console.log(data)
+                btn.classList.toggle('disabled')
+            }
+        })
+    }
 
     const getLocalStore = () => {
         const result = localStorage.getItem('result')
         return result === null ? {} : JSON.parse(result)
     }
 
-    const addBtnHandler = (event) => {
-        const { target } = event
-        if (target.classList.contains('btn-success')) {
-            let data = getLocalStore()
-            const id = event.target.getAttribute('data-id')
-            if (data.hasOwnProperty(id)) {
-                delete data.id
-                target.classList.toggle('disabled')
-            } else {
-                data[id] = target.textContent
-            }
-            localStorage.setItem('result', JSON.stringify(data))
-        }
+    const changingBtnsState = (event) => {
+        const parentElement = event.target.parentNode
+        const btns = parentElement.querySelectorAll('.btn')
+        btns.forEach((btn) => {
+            btn.classList.toggle('disabled')
+        })
     }
 
-    const deleteBtnHandler = (event) => {
+    const addBtnHandler = (event) => {
         const { target } = event
-        if (target.classList.contains('btn-danger')) {
+        if (
+            target.classList.contains('btn-success') ||
+            target.classList.contains('btn-danger')
+        ) {
             let data = getLocalStore()
             const id = event.target.getAttribute('data-id')
             if (data.hasOwnProperty(id)) {
-                delete data.id
-                target.classList.toggle('enabled')
+                for (let key in data) {
+                    if (id === key) {
+                        delete data[key]
+                    }
+                }
             } else {
                 data[id] = target.textContent
             }
+            changingBtnsState(event)
             localStorage.setItem('result', JSON.stringify(data))
         }
     }
 
     const loadedHandler = (event) => {
-        // restoreFromState()
+        restoreFromState()
         const ul = document.querySelector('[data-list-group]')
         ul.addEventListener('click', addBtnHandler)
-        ul.addEventListener('click', deleteBtnHandler)
     }
 
     document.addEventListener('DOMContentLoaded', loadedHandler)
