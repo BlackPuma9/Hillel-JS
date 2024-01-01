ba55f5ad1555bf7f904597807c2e512dea12b594
BlackPuma9 <yuliiayavorska9@gmail.com>
2024-01-01 23:25:06 +0200

    Completed hw54 + fibonacci

--- js/app.js
+++ js/app.js
@@ -1,31 +1,53 @@
 'use strict'
 ;(() => {
-    const reverseIterator = (array) => {
-        let nextIndex = array.length
-        return {
-            next: () => {
-                nextIndex--
-                const done = nextIndex < 0
-                return {
-                    value: !done ? array[nextIndex] : null,
-                    done: done,
-                }
-            },
-            [Symbol.iterator]() {
-                return this
-            },
+    // Task 1
+    // const reverseIterator = (array) => {
+    //     let nextIndex = array.length
+    //     return {
+    //         next: () => {
+    //             nextIndex--
+    //             const done = nextIndex < 0
+    //             return {
+    //                 value: !done ? array[nextIndex] : null,
+    //                 done: done,
+    //             }
+    //         },
+    //         [Symbol.iterator]() {
+    //             return this
+    //         },
+    //     }
+    // }
+    //
+    // const arr = reverseIterator([11, 12, 13, 14, 15])
+    // console.log(arr.next().value)
+    // console.log(arr.next().value)
+    // console.log(arr.next().value)
+    // console.log(arr.next().value)
+    // console.log(arr.next().value)
+    // console.log(arr.next().done)
+    //
+    // for (let item of reverseIterator([16, 17, 18, 19, 20])) {
+    //     console.log(item)
+    // }
+
+    // Task 2
+    function* fibonacci() {
+        let num1 = 0
+        let num2 = 1
+
+        while (true) {
+            let next = num1 + num2
+
+            yield num2 > 2 ? next : num2
+
+            num1 = num2
+            num2 = next
         }
     }
 
-    const arr = reverseIterator([11, 12, 13, 14, 15])
-    console.log(arr.next().value)
-    console.log(arr.next().value)
-    console.log(arr.next().value)
-    console.log(arr.next().value)
-    console.log(arr.next().value)
-    console.log(arr.next().done)
+    const sequence = fibonacci()
 
-    for (let item of reverseIterator([16, 17, 18, 19, 20])) {
-        console.log(item)
+    for (let i = 0; i < 10; i++) {
+        console.log(sequence.next())
     }
 })()
