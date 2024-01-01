2db4250cf8f903d092dc8dfa992279c335af671d
BlackPuma9 <yuliiayavorska9@gmail.com>
2024-01-02 00:00:39 +0200

    Completed hw54 + iterator Object's properties

--- js/app.js
+++ js/app.js
@@ -1,35 +1,34 @@
 'use strict'
 ;(() => {
     // Task 1
-    // const reverseIterator = (array) => {
-    //     let nextIndex = array.length
-    //     return {
-    //         next: () => {
-    //             nextIndex--
-    //             const done = nextIndex < 0
-    //             return {
-    //                 value: !done ? array[nextIndex] : null,
-    //                 done: done,
-    //             }
-    //         },
-    //         [Symbol.iterator]() {
-    //             return this
-    //         },
-    //     }
-    // }
-    //
-    // const arr = reverseIterator([11, 12, 13, 14, 15])
-    // console.log(arr.next().value)
-    // console.log(arr.next().value)
-    // console.log(arr.next().value)
-    // console.log(arr.next().value)
-    // console.log(arr.next().value)
-    // console.log(arr.next().done)
-    //
-    // for (let item of reverseIterator([16, 17, 18, 19, 20])) {
-    //     console.log(item)
-    // }
+    const reverseIterator = (array) => {
+        let nextIndex = array.length
+        return {
+            next: () => {
+                nextIndex--
+                const done = nextIndex < 0
+                return {
+                    value: !done ? array[nextIndex] : null,
+                    done: done,
+                }
+            },
+            [Symbol.iterator]() {
+                return this
+            },
+        }
+    }
 
+    const arr = reverseIterator([11, 12, 13, 14, 15])
+    console.log(arr.next().value)
+    console.log(arr.next().value)
+    console.log(arr.next().value)
+    console.log(arr.next().value)
+    console.log(arr.next().value)
+    console.log(arr.next().done)
+
+    for (let item of reverseIterator([16, 17, 18, 19, 20])) {
+        console.log(item)
+    }
     // Task 2
     function* fibonacci() {
         let num1 = 0
@@ -50,4 +49,36 @@
     for (let i = 0; i < 10; i++) {
         console.log(sequence.next())
     }
+
+    // Task 3
+    const obj = {
+        name: 'Jan',
+        age: 30,
+        hasCat: false,
+    }
+
+    const objectPropertyIterator = (obj) => {
+        const arr = Object.keys(obj)
+        let nextIndex = 0
+        return {
+            next: () => {
+                const done = nextIndex >= arr.length
+                const res = {
+                    value: !done ? arr[nextIndex] : null,
+                    done: done,
+                }
+                nextIndex++
+                return res
+            },
+            [Symbol.iterator]() {
+                return this
+            },
+        }
+    }
+
+    const keys = objectPropertyIterator(obj)
+    console.log(keys.next().value)
+    console.log(keys.next().value)
+    console.log(keys.next().value)
+    console.log(keys.next().done)
 })()
