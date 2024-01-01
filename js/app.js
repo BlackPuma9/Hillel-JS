a129ea7c083065c7ee521a7c30afd09b0a7f5f08
BlackPuma9 <yuliiayavorska9@gmail.com>
2024-01-01 22:46:01 +0200

    Completed hw54

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
 'use strict'
+;(() => {
+    const reverseIterator = (array) => {
+        let nextIndex = array.length
+        return {
+            next: () => {
+                nextIndex--
+                const done = nextIndex < 0
+                return {
+                    value: !done ? array[nextIndex] : null,
+                    done: done,
+                }
+            },
+            [Symbol.iterator]() {
+                return this
+            },
+        }
+    }
+
+    const arr = reverseIterator([11, 12, 13, 14, 15])
+    console.log(arr.next().value)
+    console.log(arr.next().value)
+    console.log(arr.next().value)
+    console.log(arr.next().value)
+    console.log(arr.next().value)
+    console.log(arr.next().done)
+
+    for (let item of reverseIterator([16, 17, 18, 19, 20])) {
+        console.log(item)
+    }
+})()
