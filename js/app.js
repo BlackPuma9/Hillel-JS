a374061b5b40efa8ce3416a4df0f2ddd037a82c6
BlackPuma9 <yuliiayavorska9@gmail.com>
2024-01-06 17:24:12 +0100

    Created first examples

--- js/app.js
+++ js/app.js
@@ -35,5 +35,36 @@ Student.prototype.absent = function () {
     this.attendance.push(false)
 }
 
+Student.prototype.getAvgAttendance = function () {
+    return (
+        this.attendance.reduce((sum, attendance) => sum + attendance, 0) /
+        this.attendance.length
+    )
+}
+
+Student.prototype.getSummary = function () {
+    if (this.getAvgGrade > 90 && this.getAvgAttendance() > 0.9) {
+        return 'Nice! Well done'
+    } else if (this.getAvgGrade() >= 60) {
+        return 'Good, but can be better'
+    } else {
+        return 'Rediska!'
+    }
+}
+
 let student1 = new Student('Yul', 'Yav', 2000, [100, 90])
-let student2 = new Student('Jan', 'Janko', 1994, [100, 90])
+let student2 = new Student('Jan', 'Janko', 1994, [40, 60])
+
+console.log(student1.getAge())
+console.log(student1.getAvgGrade())
+student1.present()
+student1.absent()
+console.log(student1.getAvgAttendance())
+console.log(student1.getSummary())
+
+console.log(student2.getAge())
+console.log(student2.getAvgGrade())
+student2.present()
+student2.absent()
+console.log(student2.getAvgAttendance())
+console.log(student2.getSummary())
