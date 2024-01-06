327bf8f81de4fcbb7faca80545e2fe496d55561f
BlackPuma9 <yuliiayavorska9@gmail.com>
2024-01-06 16:54:09 +0100

    Created Student and getAge and avgGrade methods

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
 'use strict'
+
+function Student(name, surname, birthYear, grades) {
+    this.name = name
+    this.surname = surname
+    this.birthYear = birthYear
+    this.grades = grades
+    this.attendance = []
+}
+
+Student.prototype.getAge = function () {
+    return 2024 - this.birthYear
+}
+
+Student.prototype.getAvgGrade = function () {
+    if (!this.grades.length) {
+        return 0
+    }
+    return (
+        this.grades.reduce((sum, grade) => sum + grade, 0) / this.grades.length
+    )
+}
+
+let student1 = new Student('Yul', 'Yav', 2000, [100, 90])
+let student2 = new Student('Jan', 'Janko', 1994, [100, 90])
+console.log(student1.getAvgGrade())
+console.log(student2.getAge())
