0800d757c93e6a94ce73b0a85defd9ae8bc460f1
BlackPuma9 <yuliiayavorska9@gmail.com>
2024-01-21 16:01:31 +0100

    Completed basic functional

---
+++ js/PhoneBook.js
@@ -0,0 +1,162 @@
+class PhoneBook {
+    #ATTRS = {
+        dataUserId: 'data-user-id',
+        dataRemoveBtn: 'data-remove-btn',
+        dataCallBtn: 'data-call-btn',
+        dataEndCallBtn: 'data-end-call-btn',
+    }
+    #contacts = []
+    #searchedUsers = []
+    #modalWindow = null
+    callDurationEl = null
+    constructor(users) {
+        // Validate users
+        // add users to contacts
+        users.forEach((user) => {
+            this.addContact(user)
+        })
+        this.#modalWindow = new bootstrap.Modal('#exampleModal', {
+            keyboard: false,
+            backdrop: 'static',
+        })
+        this.callDurationEl = this.#modalWindow._element.querySelector(
+            '.modal-title .duration'
+        )
+        this.#setEvents()
+    }
+
+    list(contacts) {
+        const ul = document.querySelector('.list-group')
+        const li = contacts.map((contact) =>
+            this.createContactTemplate(contact)
+        )
+        ul.innerHTML = ''
+        ul.prepend(...li)
+    }
+
+    addContact(user) {
+        // validates user and adding that to this.#contacts
+        if (user.id === null) return
+        this.#contacts.push(new User(user))
+    }
+
+    call(contactId) {
+        // find contact in this.#contacts and make a call
+        const contact = this.#contacts.find(({ id }) => id === contactId)
+
+        this.#modalWindow._element.querySelector(
+            '.modal-title .title'
+        ).innerHTML = contact.name
+        this.#modalWindow._element.querySelector('.modal-body').innerHTML =
+            'Conversation with ' + contact.phone
+        this.#modalWindow.show()
+
+        callController.startCall(contact)
+    }
+
+    removeContact(contactId) {
+        // will remove contact from this.#contacts
+        this.#contacts = this.#contacts.filter((user) => user.id !== contactId)
+        this.list(this.#contacts)
+    }
+
+    search(searchStr) {
+        // will search contact by: name, phone, email
+        const searchListResult = this.#contacts.filter(
+            (user) =>
+                user.name?.includes(searchStr) ||
+                user.phone?.includes(searchStr) ||
+                user.email?.includes(searchStr)
+        )
+
+        this.list(searchListResult)
+    }
+
+    #setEvents() {
+        document.addEventListener('DOMContentLoaded', () =>
+            this.list(this.#contacts)
+        )
+
+        const ul = document.querySelector('.list-group')
+        ul.addEventListener('click', this.#removeHandler)
+
+        const input = document.querySelector('.form-control')
+        input.addEventListener('change', this.#searchHandler)
+
+        ul.addEventListener('click', this.#callHandler)
+
+        const modal = this.#modalWindow._element.querySelector(
+            `[${this.#ATTRS.dataEndCallBtn}]`
+        )
+        modal.addEventListener('click', this.#endCall)
+    }
+
+    #removeHandler = (event) => {
+        const { target: element } = event
+        event.stopPropagation()
+        if (
+            !element
+                .closest(`[${this.#ATTRS.dataRemoveBtn}]`)
+                ?.hasAttribute(this.#ATTRS.dataRemoveBtn)
+        )
+            return
+        const userId = +element
+            .closest(`[${this.#ATTRS.dataUserId}]`)
+            .getAttribute(this.#ATTRS.dataUserId)
+        this.removeContact(userId)
+    }
+
+    #searchHandler = (event) => {
+        this.search(event.target.value)
+    }
+
+    #callHandler = (event) => {
+        const { target: element } = event
+        event.stopPropagation()
+        if (
+            !element
+                .closest(`[${this.#ATTRS.dataCallBtn}]`)
+                ?.hasAttribute(this.#ATTRS.dataCallBtn)
+        )
+            return
+        const userId = +element
+            .closest(`[${this.#ATTRS.dataUserId}]`)
+            .getAttribute(this.#ATTRS.dataUserId)
+        this.call(userId)
+    }
+
+    #endCall = () => {
+        callController.endCall()
+        this.#modalWindow.hide()
+        this.callDurationEl.innerHTML = '00:00'
+    }
+
+    createContactTemplate(user) {
+        const wrapper = document.createElement('div')
+        wrapper.className =
+            'list-group-item d-flex justify-content-between align-items-center'
+        wrapper.setAttribute('data-user-id', user.id)
+
+        wrapper.innerHTML = `<span class="contacts__contact">${user.name}</span>
+                            <div> 
+                                <button type="button" ${
+                                    this.#ATTRS.dataCallBtn
+                                } class="btn btn-success">
+                                    <i class="bi bi-telephone"></i>
+                                </button>
+
+                                <button type="button" ${
+                                    this.#ATTRS.dataRemoveBtn
+                                } class="btn btn-danger">
+                                    <i class="bi bi-trash"></i>
+                                </button>
+                            </div>`
+
+        return wrapper
+    }
+
+    // your methods
+    // All event handlers should be a separate private methods
+}
+
+const phoneBook = new PhoneBook(users)
---
+++ js/User.js
@@ -0,0 +1,39 @@
+class User {
+    #id = null
+    #name = null
+    #phone = null
+    #email = null
+    #website = null
+
+    constructor({ id, name, phone, email, website }) {
+        this.#id = id
+        this.#name = name
+        this.#phone = phone
+        this.#email = email
+        this.#website = website
+    }
+
+    static isUser(obj) {
+        // check if obj is User instance
+    }
+
+    get id() {
+        return this.#id
+    }
+
+    get name() {
+        return this.#name
+    }
+
+    get phone() {
+        return this.#phone
+    }
+
+    get email() {
+        return this.#email
+    }
+
+    get website() {
+        return this.#website
+    }
+}
