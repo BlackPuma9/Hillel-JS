7cc4044dfd716ae9ec66459d454f1c599974323f
BlackPuma9 <yuliiayavorska9@gmail.com>
2024-02-07 22:56:00 +0100

    Completed hw66

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 'use strict'
+
+function analyzeText(phrase) {
+    const newArr = phrase
+        .toLowerCase()
+        .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '')
+        .split(' ')
+    const uniqueSet = new Set(newArr)
+    return {
+        uniqueWordsCount: uniqueSet.size,
+        uniqueWords: [...uniqueSet],
+    }
+}
+const result = analyzeText('Hi Dear, my name Dear dear Olha hi my!')
+console.log(result.uniqueWordsCount)
+console.log(result.uniqueWords)
