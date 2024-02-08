61f393d63d09c5ac8edb9d267e4b66b25bf19f59
BlackPuma9 <yuliiayavorska9@gmail.com>
2024-02-08 20:16:23 +0100

    Completed hw68

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
 'use strict'
+
+const storage = new WeakSet()
+function trackObjects(obj) {
+    if (!storage.has(obj)) {
+        storage.add(obj)
+        return false
+    }
+    return true
+}
+
+const obj = { name: 'Yul' }
+const result1 = trackObjects(obj)
+const result2 = trackObjects(obj)
+const result3 = trackObjects({ name: 'Alla' })
+
+console.log(result1)
+console.log(result2)
+console.log(result3)
+console.log(storage)
