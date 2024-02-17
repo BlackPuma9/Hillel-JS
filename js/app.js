ca44c777f7571ebbe15b1aeaec1be698f96da106
BlackPuma9 <yuliiayavorska9@gmail.com>
2024-02-17 20:18:07 +0100

    Completed hw70

--- js/app.js
+++ js/app.js
@@ -1,1 +1,36 @@
 'use strict'
+
+const searchParams = new URLSearchParams({
+    q: 'Kyiv',
+    units: 'metric',
+    APPID: '5d066958a60d315387d9492393935c19',
+})
+
+fetch(`https://api.openweathermap.org/data/2.5/weather?${searchParams}`)
+    .then((response) => response.json())
+    .then((data) => {
+        const body = document.querySelector('body')
+        const div = document.createElement('div')
+        if (data.weather.length === 0) {
+            div.innerHTML = 'Can not load weather data'
+            body.append(div)
+            return
+        }
+        div.innerHTML = `<p>
+                            <img 
+                                src='http://openweathermap.org/img/w/${data.weather[0].icon}.png' 
+                                alt='Weather icon'
+                                width="50" 
+                                height="50"
+                            >
+                         </p>
+                         <p>Your city: ${data.name}</p>
+                         <p>Temperature: ${data.main.temp}</p>
+                         <p>Pressure: ${data.main.pressure}</p> 
+                         <p>Description: ${data.weather[0].description}</p> 
+                         <p>Humidity: ${data.main.humidity}</p> 
+                         <p>Wind Speed: ${data.wind.speed}</p> 
+                         <p>Wind Deg: ${data.wind.deg}</p> 
+                         `
+        body.append(div)
+    })
